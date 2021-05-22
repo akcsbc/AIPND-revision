256f1408b0de13a0e06286d4ae8bd8f348cc0c40
akcs <simonstrzoda@gmail.com>
2021-05-22 23:31:02 +0200

    update programmer and date

--- intropyproject-classify-pet-images/print_results_hints.py
+++ intropyproject-classify-pet-images/print_results_hints.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 # */AIPND-revision/intropyproject-classify-pet-images/print_results_hints.py
 #                                                                             
-# PROGRAMMER: 
-# DATE CREATED:
+# PROGRAMMER: Szymon Strzoda
+# DATE CREATED: 22.05.2021
 # REVISED DATE: 
 # PURPOSE: This is a *hints* file to help guide students in creating the 
 #          function print_results that prints the results statistics from the
